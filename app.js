a30003073b70d092ddc30e186d77a455fbe40fad
Elvin251 <emmmdov485@gmail.com>
2024-11-23 22:46:28 +0400

    d

---
+++ app.js
@@ -0,0 +1,42 @@
+let rayon= prompt("Rayon daxil edin")
+switch (rayon) {
+    case "Cəbrayıl":
+        console.log("04.10.2020")
+        break;
+        case "Füzuli":
+            console.log("17.10.2020")
+            break;
+            case "Zəngilan":
+                console.log("20.10.2020")
+                break;
+                case "Qubadlı":
+                    console.log("25.10.2020")
+                    break;
+                    case "Şuşa":
+                        console.log("08.11.2020")
+                        break;
+                        case "Xankəndi":
+                            console.log("10.11.2020")
+                            break;
+                            case "Xocalı":
+                                console.log("10.11.2020")
+                                break;
+                                case "Xocavənd":
+                                    console.log("10.11.2020")
+                                    break;
+                                    case "Ağdərə":
+                                        console.log("10.11.2020")
+                                        break;
+                                        case "Ağdam":
+                                            console.log("20.11.2020")
+                                            break;
+                                            case "Kəlbəcər":
+                                                console.log("25.11.2020")
+                                                break;
+                                                case "Laçın":
+                                                    console.log("01.12.2020")
+                                                    break;
+
+    default:
+        break;
+}
